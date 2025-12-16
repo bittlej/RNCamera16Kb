4cf9a60fe8c17fc2612e24196f6ca190e6375f1f
bittlej <bittlej@gmail.com>
2025-12-16 14:59:17 -0500

    trying another TextRecognizer import

--- android/src/mlkit/java/org/reactnative/camera/tasks/TextRecognizerAsyncTask.java
+++ android/src/mlkit/java/org/reactnative/camera/tasks/TextRecognizerAsyncTask.java
@@ -17,7 +17,8 @@ import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+//import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
 
 import org.reactnative.camera.utils.ImageDimensions;
 
@@ -70,8 +71,8 @@ public class TextRecognizerAsyncTask extends android.os.AsyncTask<Void, Void, Vo
     if (isCancelled() || mDelegate == null) {
       return null;
     }
-	TextRecognizerOptionsInterface txtRecog;
-    TextRecognizer detector = TextRecognition.getClient(txtRecog);
+	//TextRecognizerOptionsInterface txtRecog;
+    TextRecognizer detector = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
 
     InputImage image = InputImage.fromByteArray(mImageData, mWidth, mHeight, getFirebaseRotation(), InputImage.IMAGE_FORMAT_YV12);
     detector.process(image)
